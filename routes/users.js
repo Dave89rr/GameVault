53fdffa7e9a15c804ab7a3de5c26aa58856d33d5
jiaqicheng1998 <jiaqicheng1998@gmail.com>
2022-06-06 13:42:28 -0700

    sign-up pug

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,19 @@
 var express = require('express');
+const { check, validationResult } = require('express-validator');
+
+const db = require('../db/models');
 var router = express.Router();
+const { csrfProtection, asyncHandler } = require('./utils');
+
+router.get('/sign-up', csrfProtection, (req, res) => {
+  const user = db.User.build();
+  res.render('user-register', {
+    title: 'Register',
+    user,
+    csrfToken: req.csrfToken(),
+  });
+});
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
