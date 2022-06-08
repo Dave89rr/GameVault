4571f7b79ed8107e040409ce96ec3c24b94f21a9
Angie Maidt <angiemaidt@gmail.com>
2022-06-08 13:53:25 -0600

    linked logo->home, username->profile, addNewCollection button->/collections/new

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,10 @@
-window.addEventListener("load", (event)=>{
-    console.log("hello from javascript!")
+
+
+window.addEventListener("DOMContentLoaded", async(event)=>{
+    document.getElementById('new-collection').addEventListener('click', (e) => {
+        const res = await fetch('/collections/new');
+        return res;
+        // window.location.href = '/collections/new';
+    })
+
 })
