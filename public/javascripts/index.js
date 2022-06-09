7e2630c540d50f4af5276d8d3bba4e6ff5b23bff
David Rivera <daleriro@gmail.com>
2022-06-08 20:31:54 -0400

    Patches Added links and functionality to various buttons fixed bug in dynamic rendering of new game added to collection

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -7,15 +7,39 @@ document
     const selectValue = document.getElementById('selectValue').value;
     const bodyObj = {
       game_id: selectValue,
-      played_status_id: 1,
       collection_id: collectionID,
     };
     const jsonBody = JSON.stringify(bodyObj);
-    const res = await fetch(`${url}/entries/`, {
+    const catchResponse = await fetch(`${url}/entries/`, {
       method: 'post',
       headers: {
         'Content-Type': 'application/json',
       },
       body: jsonBody,
     });
+
+    const data = await catchResponse.json();
+
+    if (data.message === 'db updated') {
+      const card = document.createElement('div');
+      const cardTitle = document.createElement('div');
+      card.setAttribute('class', 'card');
+      card.innerHTML = `<div class='game-title'>
+      <p>${data.game.name}</p>
+      </div>
+      <a href='/games/${data.game.id}'>
+      <div class='home-img' style='background-image: url(${data.game.img_url});'></div>
+      </a>
+      `;
+
+      /*   div(class='card')
+    .game-title
+      p= game.name
+    a(href=`/games/${game.id}`)  
+      div(class='home-img' style=`background-image: url(${game.img_url});`) */
+
+      const imageBox = document.querySelector('.img-box');
+
+      imageBox.appendChild(card);
+    }
   });
--- routes/entries.js
+++ routes/entries.js
@@ -6,25 +6,32 @@ const { requireAuth } = require('../auth');
 
 const router = express.Router();
 
-router.post('/', /*requireAuth*/ asyncHandler(async (req, res) => {
-    const { game_id } = req.body;
-    const collection_id = parseInt(req.originalUrl.split('/')[2], 10);
+router.post(
+  '/',
+  /*requireAuth*/ asyncHandler(async (req, res) => {
+    const { game_id, collection_id } = req.body;
 
-    const collection = await db.Collection.findByPk(collection_id);
     const entry = await db.Entry.create({
-        game_id: game_id,
-        played_status_id: 1,
-        collection_id: collection_id
+      game_id: game_id,
+      played_status_id: 1,
+      collection_id: collection_id,
     });
-    //res.send('it is done')
+    const game = await db.Game.findByPk(game_id);
 
-    res.render('collection', { collection });
-}));
+    res.send({ message: 'db updated', game });
 
-router.delete('/:id(\\d+)', asyncHandler(async (req, res) => {
-    const entryId = parseInt(req.params.id,10);
-    await db.Entry.destroy({where: {id: entryId}});
-    res.send('it is deleted')
-}))
+    // res.render('collection', { collection });
+    // res.send('hi');
+  })
+);
+
+router.delete(
+  '/:id(\\d+)',
+  asyncHandler(async (req, res) => {
+    const entryId = parseInt(req.params.id, 10);
+    await db.Entry.destroy({ where: { id: entryId } });
+    res.send('it is deleted');
+  })
+);
 
 module.exports = router;
