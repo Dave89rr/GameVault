84f555aea422594763de880cc0c3a4578b0c67b0
David Rivera <daleriro@gmail.com>
2022-06-09 17:26:36 -0400

    Removing Console Logs Fixed seeder img url

--- public/javascripts/postReview.js
+++ public/javascripts/postReview.js
@@ -1,30 +1,25 @@
-document.getElementById("postReview")
-        .addEventListener('click', async (e) => {
-            e.preventDefault();
-            const url = window.location.pathname;
-            //console.log(url)
-            const gameId = url.split('/')[2];
-            const content = document.getElementById('content').value;
-            const ratings = document.getElementById('ratings').value
-            //console.log(content);
-            const jsonBody = JSON.stringify({ content, ratings });
-            const catchResponse = await fetch(`${url}/reviews`, {
-                method: "post",
-                headers: { "Content-Type":"application/json"},
-                body: jsonBody
-            });
-            //console.log(catchResponse)
+document.getElementById('postReview').addEventListener('click', async (e) => {
+  e.preventDefault();
+  const url = window.location.pathname;
+  const gameId = url.split('/')[2];
+  const content = document.getElementById('content').value;
+  const ratings = document.getElementById('ratings').value;
+  const jsonBody = JSON.stringify({ content, ratings });
+  const catchResponse = await fetch(`${url}/reviews`, {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: jsonBody,
+  });
 
-            const data = await catchResponse.json()
-            //console.log(data);
-            if (data.message === 'review ok') {
-                const indReview = document.createElement('div');
-                indReview.setAttribute('class', 'ind-review');
-                indReview.innerHTML = `
+  const data = await catchResponse.json();
+  if (data.message === 'review ok') {
+    const indReview = document.createElement('div');
+    indReview.setAttribute('class', 'ind-review');
+    indReview.innerHTML = `
                 <p>${data.user.username} rated <span>${data.review.stars}<span></p>
                 <p>${data.review.content}</p>
-                `
-                const ul = document.querySelector('ul');
-                ul.appendChild(indReview)
-            }
-        })
+                `;
+    const ul = document.querySelector('ul');
+    ul.appendChild(indReview);
+  }
+});
--- routes/reviews.js
+++ routes/reviews.js
@@ -6,58 +6,73 @@ var router = express.Router();
 const { csrfProtection, asyncHandler } = require('./utils');
 const { requireAuth } = require('../auth');
 
-router.get('/:id(\\d+)', asyncHandler(async (req, res) => { //individual game page
-    const gameId = req.params.id
-    const game = await db.Game.findOne({ include: 'Reviews', where: { id: gameId }})
+router.get(
+  '/:id(\\d+)',
+  asyncHandler(async (req, res) => {
+    //individual game page
+    const gameId = req.params.id;
+    const game = await db.Game.findOne({
+      include: 'Reviews',
+      where: { id: gameId },
+    });
 
-    const reviews = await db.Review.findAll({include: 'User', where: {game_id: gameId}});
-    //console.log(reviews[0].User)
-    let loggedInUser
-    let userCollections
+    const reviews = await db.Review.findAll({
+      include: 'User',
+      where: { game_id: gameId },
+    });
+    let loggedInUser;
+    let userCollections;
     if (req.session.auth) {
-        loggedInUser = req.session.auth.userId
+      loggedInUser = req.session.auth.userId;
     }
 
     if (loggedInUser) {
-        userCollections = await db.Collection.findAll({
-            where: {
-                user_id: loggedInUser
-            }
-        })
+      userCollections = await db.Collection.findAll({
+        where: {
+          user_id: loggedInUser,
+        },
+      });
     } else {
-        userCollections = null;
+      userCollections = null;
     }
 
     res.render('game', {
-        game: game,
-        reviews: reviews,
-        loggedInUser: loggedInUser,
-        userCollections: userCollections
-    })
-}))
+      game: game,
+      reviews: reviews,
+      loggedInUser: loggedInUser,
+      userCollections: userCollections,
+    });
+  })
+);
 
-router.post('/:id(\\d+)/reviews', requireAuth, asyncHandler(async (req, res) => {
+router.post(
+  '/:id(\\d+)/reviews',
+  requireAuth,
+  asyncHandler(async (req, res) => {
     const { content, ratings } = req.body;
-    console.log(req.body);
     const gameId = parseInt(req.params.id, 10);
     const userId = res.locals.user.id;
 
     const review = await db.Review.create({
-        game_id: gameId,
-        user_id: userId,
-        content: content,
-        stars: ratings
-    })
+      game_id: gameId,
+      user_id: userId,
+      content: content,
+      stars: ratings,
+    });
 
-    const user = await db.User.findByPk(userId)
-    res.send({ message: 'review ok', review, user});
-}));
+    const user = await db.User.findByPk(userId);
+    res.send({ message: 'review ok', review, user });
+  })
+);
 
-router.delete('/:id(\\d+)/reviews/:reviewId(\\d+)', asyncHandler(async (req, res) => {
-    const reviewId = req.params.reviewId
-    const review = await db.Review.findByPk(reviewId)
-    await review.destroy()
-    res.send({ message: 'deleted' })
-}))
+router.delete(
+  '/:id(\\d+)/reviews/:reviewId(\\d+)',
+  asyncHandler(async (req, res) => {
+    const reviewId = req.params.reviewId;
+    const review = await db.Review.findByPk(reviewId);
+    await review.destroy();
+    res.send({ message: 'deleted' });
+  })
+);
 
 module.exports = router;
