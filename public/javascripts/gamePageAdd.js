9f6bb99b62350d7a40bb7bbaaea733ac5fa5348e
Angie Maidt <angiemaidt@gmail.com>
2022-06-09 13:47:24 -0600

    added window.alert when user adds game to collection from /games/:gameId

--- public/javascripts/gamePageAdd.js
+++ public/javascripts/gamePageAdd.js
@@ -1,6 +1,24 @@
-// document.getElementById('gamePageAdd').addEventListener('click', async (e) => {
-//     e.preventDefault();
-//     const url = window.location.pathname;
-//     const game_id = url.split('/')[2];
-//     const collection
-// })
+document.getElementById('gamePageAdd').addEventListener('click', async (e) => {
+    e.preventDefault();
+    const url = window.location.pathname;
+    const game_id = url.split('/')[2];
+    const collection_id = document.getElementById('collection_id').value;
+    const bodyObj = {
+        game_id: game_id,
+        collection_id: collection_id
+    };
+    const jsonBody = JSON.stringify(bodyObj);
+    const catchResponse = await fetch(`/collections/${collection_id}/entries/`, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: jsonBody
+    });
+
+    const data = await catchResponse.json();
+
+    if(data.message === 'db updated') {
+        window.alert('Game added to collection');
+    }
+})
