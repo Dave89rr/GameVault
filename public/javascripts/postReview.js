79abc783fd39cbc3ef30b31b6f08bce2d24b4925
David Rivera <daleriro@gmail.com>
2022-06-10 11:55:46 -0400

    Dynamically adding delete button to reviews

--- public/javascripts/postReview.js
+++ public/javascripts/postReview.js
@@ -3,6 +3,7 @@ document.getElementById('postReview').addEventListener('click', async (e) => {
   const url = window.location.pathname;
   const gameId = url.split('/')[2];
   const content = document.getElementById('content').value;
+  const textarea = document.getElementById('content');
   const ratings = document.getElementById('ratings').value;
   const jsonBody = JSON.stringify({ content, ratings });
   const catchResponse = await fetch(`${url}/reviews`, {
@@ -18,8 +19,29 @@ document.getElementById('postReview').addEventListener('click', async (e) => {
     indReview.innerHTML = `
                 <p>${data.user.username} rated <span>${data.review.stars}<span></p>
                 <p>${data.review.content}</p>
+                <button id='delete-review-${data.review.id}'>Delete</button>
                 `;
+    // button(class="delete-btn" id=`delete-review-${review.id}`) Delete
+    // button(class="edit-btn" id=`edit-review-${review.id}`) Edit
     const ul = document.querySelector('ul');
     ul.appendChild(indReview);
+    textarea.value = '';
+    document
+      .getElementById(`delete-review-${data.review.id}`)
+      .addEventListener('click', async (e) => {
+        e.preventDefault();
+        const url = window.location.pathname;
+        const gameId = url.split('/')[2];
+        const reviewId = e.target.id.split('-')[2];
+        const fetchUrl = `/games/${gameId}/reviews/${reviewId}`;
+        const res = await fetch(fetchUrl, {
+          method: 'DELETE',
+        });
+
+        const data = await res.json();
+        if ((data.message = 'deleted')) {
+          indReview.remove();
+        }
+      });
   }
 });
