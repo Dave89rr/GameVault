4c435121132d876058d854ba7eab87d592b8fe9f
Anai Lopez <anaijocelyn@gmail.com>
2022-06-12 18:54:33 -0700

    game can now be removed  after being added to collection(no refresh needed)

--- public/javascripts/addGameToColl.js
+++ public/javascripts/addGameToColl.js
@@ -67,6 +67,27 @@ document
             }),
           });
         });
+
+      const deleteButtons = document.querySelectorAll('.delete-button');
+
+      for (i = 0; i < deleteButtons.length; i++) {
+        const deleteBttn = deleteButtons[i];
+
+        deleteBttn.addEventListener('click', async (e) => {
+          e.preventDefault();
+          const gameId = e.target.id.split('-')[1];
+
+          const res = await fetch(`/entries/${gameId}`, {
+            method: 'DELETE',
+          });
+
+          const data = await res.json();
+          if (data.message === 'it is deleted') {
+            const card = document.getElementById(`card-${gameId}`);
+            card.remove();
+          }
+        });
+      }
     } else {
       alert('Game is already in your collection');
     }
